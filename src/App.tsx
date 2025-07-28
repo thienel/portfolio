2a0f34871bb44bb4e53aa020df4c636cd1d5d028
thienel <lethien.dev@gmail.com>
2025-07-29 00:41:35 +0700

    feat: define routes and add MainPortfolio to Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Welcome from '@components/Welcome';
 import WebGL from './webgl';
+import MainPortfolio from '@components/MainPortfolio';
 
 function App() {
   const [isWelcomeComplete, setIsWelcomeComplete] = useState(false);
 
-
   useEffect(() => {
-    WebGL()
+    WebGL();
   }, []);
 
   return (
-    <div className="main-container">
-
-      {
-        !isWelcomeComplete &&
-      <Welcome onComplete={() => setIsWelcomeComplete(true)} />
-      }
-      <div id="home"></div>
-      <canvas className="webgl"></canvas>
-
-      <input type="text" id="textarea" readOnly />
-    </div>
+    <BrowserRouter>
+      <div className="main-container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {!isWelcomeComplete && (
+                  <Welcome onComplete={() => setIsWelcomeComplete(true)} />
+                )}
+                <div id="home"></div>
+                <canvas className="webgl"></canvas>
+                <input type="text" id="textarea" readOnly />
+              </>
+            }
+          />
+          <Route path="/portfolio" element={<MainPortfolio />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
