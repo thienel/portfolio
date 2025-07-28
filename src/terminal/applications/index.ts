8e8756b2f4a8845589e8aba6634b732f0fe5f733
thienel <lethien.dev@gmail.com>
2025-07-28 21:38:19 +0700

    chore(terminal): register cat, clear and help commands in applications

--- src/terminal/applications/index.ts
+++ src/terminal/applications/index.ts
@@ -1,13 +1,26 @@
-import { ApplicationDefinition, FileSystemNode, FileSystemPath, PrintFunction } from '../types'
+import {
+  ApplicationDefinition,
+  ApplicationRegistry,
+  FileSystemNode,
+  FileSystemPath,
+  PrintFunction,
+} from '../types'
 import cd from './cd'
 import ls from './ls'
+import cat from './cat'
+import clear from './clear'
+import help from './help'
 
 export function registerApplications(
   registerApplication: (name: string, definition: ApplicationDefinition) => void,
   print: PrintFunction,
   path: FileSystemPath,
   rootNode: FileSystemNode,
+  getApplications: () => ApplicationRegistry,
 ): void {
   registerApplication('cd', cd(print, path, rootNode))
   registerApplication('ls', ls(print, path))
+  registerApplication('cat', cat(print, path, rootNode))
+  registerApplication('clear', clear(print))
+  registerApplication('help', help(print, getApplications))
 }
