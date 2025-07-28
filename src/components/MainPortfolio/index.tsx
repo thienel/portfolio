f0ac87608417a911becb64c878cc476260d30ea3
thienel <lethien.dev@gmail.com>
2025-07-29 00:39:58 +0700

    feat(MainPortfolio): create component

---
+++ src/components/MainPortfolio/index.tsx
@@ -0,0 +1,18 @@
+import styles from './MainPortfolio.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+function MainPortfolio() {
+    return (
+    <div className={cx('wrapper')}>
+        <div className={cx('description')}>
+        <p>Hi, I'm Le Hung Thien – a junior IT student at FPT University and a back-end developer currently working with Go.</p>
+        <p>I enjoy learning through books and hands-on research, and I'm actively looking for an internship to apply my skills and gain real-world experience.</p>
+        <p>If you’re here, maybe by chance or interest, feel free to check out my resume and explore the projects I’ve built.  </p>
+        </div>
+    </div>
+    );
+}
+
+export default MainPortfolio;
