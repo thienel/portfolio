781d862b70a3e0b06fc30fdbcce92277f950b729
thienel <lethien.dev@gmail.com>
2025-07-30 00:06:47 +0700

    chore(component): organize created components to MainPortfolio

--- src/components/MainPortfolio/index.tsx
+++ src/components/MainPortfolio/index.tsx
@@ -4,15 +4,17 @@ import classNames from 'classnames/bind'
 import NavBar from './NavBar'
 import ParticleWaveBackground from '@components/ParticleWaveBackground'
 import Projects from './Projects'
+import Certificates from './Certificates'
+import Contact from './Contact'
 
 const cx = classNames.bind(styles)
 
 function MainPortfolio() {
   return (
-    <div className={cx('wrapper')}>
+    <div className={cx('wrapper')} id="main-portfolio">
+      <ParticleWaveBackground />
       <Header />
       <NavBar />
-      <ParticleWaveBackground />
       <div className={cx('description')}>
         <p>
           Hi, I'm Le Hung Thien – a junior IT student at FPT University and a back-end developer
@@ -23,11 +25,21 @@ function MainPortfolio() {
           internship to apply my skills and gain real-world experience.
         </p>
         <p>
-          If you’re here, maybe by chance or interest, feel free to check out my resume and explore
-          the projects I’ve built.
+          If you're here, maybe by chance or interest, feel free to check out my{' '}
+          <a
+            href={'/resume/LeHungThien-GolangDeveloper-Resume.pdf'}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: 'underline' }}
+          >
+            Resume
+          </a>{' '}
+          and explore the projects I've built.
         </p>
       </div>
       <Projects />
+      <Certificates />
+      <Contact />
     </div>
   )
 }
