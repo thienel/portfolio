f3a504497285fb55f87e7347a837d8086c4db43e
thienel <lethien.dev@gmail.com>
2025-07-30 16:53:01 +0700

    feat(NavBar): change functionality and styles

--- src/components/MainPortfolio/NavBar/index.tsx
+++ src/components/MainPortfolio/NavBar/index.tsx
@@ -4,42 +4,28 @@ import styles from './NavBar.module.scss'
 const cx = classNames.bind(styles)
 
 function NavBar() {
-  const scrollToSection = (sectionId: string) => {
-    const element = document.getElementById(sectionId)
-    const scrollContainer = document.getElementById('main-portfolio')
-
-    if (element && scrollContainer) {
-      const offset = 100
-      const y = element.offsetTop - offset
-      scrollContainer.scrollTo({ top: y, behavior: 'smooth' })
-    }
-  }
-
-  const scrollToTop = () => {
-    const scrollContainer = document.getElementById('main-portfolio')
-    scrollContainer?.scrollTo({ top: 0, behavior: 'smooth' })
-  }
-
-  const openGithub = () => {
-    window.open('https://github.com/thienel', '_blank', 'noopener,noreferrer')
-  }
-
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('nav-item')} onClick={scrollToTop}>
-        Home
+      <div className={cx('logo')}>
+        <a href="https://www.lethien.dev/portfolio">Le Thien</a>
       </div>
-      <div className={cx('nav-item')} onClick={() => scrollToSection('projects')}>
-        Projects
+      <div className={cx('socials')}>
+        <a href="https://github.com/thienel" target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/lehungthien" target="_blank" rel="noopener noreferrer">
+          LinkedIn
+        </a>
+        <a
+          href="resume/LeHungThien-GolangDeveloper-Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume
+        </a>
       </div>
-      <div className={cx('nav-item')} onClick={() => scrollToSection('certificates')}>
-        Certificates
-      </div>
-      <div className={cx('nav-item')} onClick={() => scrollToSection('contact')}>
-        Contact
-      </div>
-      <div className={cx('nav-item')} onClick={openGithub}>
-        Github
+      <div className={cx('email')}>
+        <a href="mailto:lethien.dev@gmail.com">lethien.dev@gmail.com</a>
       </div>
     </div>
   )
