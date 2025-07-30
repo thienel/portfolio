75f248b0a96cf2e3213711cb7d23e2482b172610
thienel <lethien.dev@gmail.com>
2025-07-30 22:33:16 +0700

    chore: add techs logo and export to Component

---
+++ src/assets/logo-svg/index.tsx
@@ -0,0 +1,25 @@
+import { FC, SVGProps } from 'react'
+
+import Cs from './dotnet.svg?react'
+import Go from './go.svg?react'
+import Js from './js.svg?react'
+import MySQL from './mysql.svg?react'
+import PostgreSQL from './postgresql.svg?react'
+import SqlServer from './sqlserver.svg?react'
+import Ts from './typescript.svg?react'
+import WebGL from './webgl.svg?react'
+import ReactIcon from './react.svg?react'
+
+const logoList = {
+  Cs,
+  Go,
+  Js,
+  MySQL,
+  PostgreSQL,
+  SqlServer,
+  Ts,
+  WebGL,
+  React: ReactIcon,
+} as const satisfies Record<string, FC<SVGProps<SVGSVGElement>>>
+
+export default logoList
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   css: {
     preprocessorOptions: {
       scss: {
